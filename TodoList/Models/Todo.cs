26a0e24700a726807e2cb2eb91e3437d7b9235f3
hryhorskyi <martin.hryhorskyi@gmail.com>
2023-06-25 14:11:29 +0300

    init

---
+++ TodoList/Models/Todo.cs
@@ -0,0 +1,11 @@
+using System;
+namespace TodoList.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+    }
+}
+
---
+++ TodoList/Models/TodoContext.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoList.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
+
